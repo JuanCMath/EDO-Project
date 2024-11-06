ed86b4ba938a699b89fb459ee88f80bb3daf1588
Juan <juancarlosmatcom@gmail.com>
2024-11-05 23:51:41 -0500

    Implementado la solucion de ecuaciones y su graficacion
    Falta el manejo de error y mostrarlo a los usuarios

--- Graficador/Euler_Runge_Kutta.py
+++ Graficador/Euler_Runge_Kutta.py
@@ -2,14 +2,6 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt  
 
-# Definición de la función derivada  
-def f(t, y):  
-    return y + t  
-
-# Solución exacta  
-def exact_solution(t):  
-    return np.exp(t) - t - 1  
-
 # Método de Euler Mejorado  
 def euler_improved(f, t0, y0, h, n):  
     t_values = [t0]  
@@ -54,7 +46,7 @@ def runge_kutta_4(f, t0, y0, h, n):
     return t_values, y_values  
 
 # Función para graficar el campo de isoclinas
-def plot_isoclines(f, t_range, y_range, num_points=50):
+def plot_isoclines(f, t_range, y_range, num_points=30):
     t = np.linspace(t_range[0], t_range[1], num_points)
     y = np.linspace(y_range[0], y_range[1], num_points)
     T, Y = np.meshgrid(t, y)
--- main.py
+++ main.py
@@ -5,17 +5,52 @@ from flet.matplotlib_chart import MatplotlibChart
 from Graficador import Euler_Runge_Kutta
 import sympy as sp
 
+# Global figure and axis
+fig, ax = plt.subplots()
+
 def main(page: ft.Page):
     # Buttons functionality
-    def solving( #TODO: Add the solution to the problem
+    def solving(
         derivativeAsString,
         xConditionAsString,
         yConditionAsString,
-        hStepAsString,
-        valueToCalculateAsString,):
+        hStepAsString,):
 
-        solution.focus()
-        solution.value = "*PERFECT SOLUTION*"  # Put the solution here
+        # Getting the function from the input
+        x, y = sp.symbols('x y')
+        user_function = sp.sympify(derivativeAsString.value)
+        f = sp.lambdify((x, y), user_function, 'numpy')
+
+        # Getting the inputs Value
+        x_val = float(xConditionAsString.value)
+        y_val = float(yConditionAsString.value)
+        h = float(hStepAsString.value)
+
+        # Exact solution
+        t_values_exact, y_values_exact = Euler_Runge_Kutta.exact_solution(f ,x_val, y_val)
+
+        # Euler Improved
+        t_values_euler, y_values_euler = Euler_Runge_Kutta.euler_improved(f, x_val, y_val, h, 20)
+
+        # Runge_kutta_4
+        t_values_runge_kutta, y_values_runge_kutta = Euler_Runge_Kutta.runge_kutta_4(f, x_val, y_val, h, 20)
+
+        # Plot the results
+        ax.plot(t_values_exact, y_values_exact, label='Solucion Exacta', color='green', linewidth=2)
+        ax.plot(t_values_euler, y_values_euler, label='Euler Mejorado', marker='o', markersize=4, linestyle='--', color='blue')
+        ax.plot(t_values_runge_kutta, y_values_runge_kutta, label='Runge-Kutta 4º Orden', marker='x', markersize=6, linestyle=':', color='red')
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_title("Solución de EDO")
+        ax.grid(True)
+        ax.legend()
+
+        # Set axis limits
+        ax.set_xlim(-10, 10)
+        ax.set_ylim(-10, 10)
+
+        # Adding the graph to the display
+        graph_container.content = MatplotlibChart(fig, expand=True)
         page.update()
 
     def createGraph():
@@ -24,7 +59,6 @@ def main(page: ft.Page):
         x = np.linspace(-100, 100, 500)
         y = x - x
 
-        fig, ax = plt.subplots()
         ax.plot(x, y, color="blue")
 
         ax.set_xlabel("x")
@@ -39,44 +73,16 @@ def main(page: ft.Page):
         graph_container.content = MatplotlibChart(fig, expand=True)
         page.update()
 
-    def onGraphingClick(
-        derivativeAsString,
-        xConditionAsString,
-        yConditionAsString,
-        hStepAsString,
-        valueToCalculateAsString,
-    ):
+    def onGraphingClick(derivativeAsString):
         # Getting the function from the input
         x, y = sp.symbols('x y')
         user_function = sp.sympify(derivativeAsString.value)
         f = sp.lambdify((x, y), user_function, 'numpy')
 
-        # Getting the inputs Value
-        x_val = float(xConditionAsString.value)
-        y_val = float(yConditionAsString.value)
-        h = float(hStepAsString.value)
-        n = float(valueToCalculateAsString.value)
-
-
-        # Exact solution #TODO
-        t_values_exact, y_values_exact = Euler_Runge_Kutta.exact_solution(f ,x_val, y_val)
-
-        #Euler Improved
-        t_values_euler, y_values_euler = Euler_Runge_Kutta.euler_improved(f, x_val, y_val, h, 20)
-
-        # Runge_kutta_4
-        t_values_runge_kutta, y_values_runge_kutta = Euler_Runge_Kutta.runge_kutta_4(f, x_val, y_val, h, 20)
-
-
-        #Isoclinas
+        # Isoclinas
         T, Y, U, V = Euler_Runge_Kutta.plot_isoclines(f, (-10, 10), (-10, 10))
 
         # Plot the results
-        fig, ax = plt.subplots()
-
-        ax.plot(t_values_exact, y_values_exact, label='Solucion Exacta', color='green', linewidth=2)
-        ax.plot(t_values_euler, y_values_euler, label='Euler Mejorado', marker='o', markersize=4, linestyle='--', color='blue')
-        ax.plot(t_values_runge_kutta, y_values_runge_kutta, label='Runge-Kutta 4º Orden', marker='x', markersize=6, linestyle=':', color='red')
         ax.quiver(T, Y, U, V, color='gray', alpha=0.5)
         ax.set_xlabel("x")
         ax.set_ylabel("y")
@@ -92,17 +98,27 @@ def main(page: ft.Page):
         graph_container.content = MatplotlibChart(fig, expand=True)
         page.update()
 
+    def reset_graph():
+        ax.clear()
+        createGraph()
+
     # Equations input
-    solution = ft.Text(value="Resultado:", width=400)
     tb1 = ft.TextField(label="y'", width=400)
     tb2 = ft.TextField(label="x0", width=400)
     tb3 = ft.TextField(label="y0", width=400)
     tb4 = ft.TextField(label="Paso h", width=400)
-    tb5 = ft.TextField(label="Calcular y en x", width=400)
-    solvingButton = ft.ElevatedButton(text="Resolver", on_click=solving)
+
+    solvingButton = ft.ElevatedButton(
+        text="Resolver", 
+        on_click=lambda ignoredParameter: solving(tb1, tb2, tb3, tb4)
+    )
     graphingButton = ft.ElevatedButton(
         text="Graficar",
-        on_click=lambda ignoredParameter: onGraphingClick(tb1, tb2, tb3, tb4, tb5),
+        on_click=lambda ignoredParameter: onGraphingClick(tb1),
+    )
+    resetButton = ft.ElevatedButton(
+        text="Reset",
+        on_click=lambda ignoredParameter: reset_graph(),
     )
     # Create a container for the graph
     graph_container = ft.Container(width=1000, height=700, alignment=ft.alignment.center)
@@ -110,7 +126,7 @@ def main(page: ft.Page):
     # Layout the components
     input_column = ft.Column(
         [
-            tb1, tb2, tb3, tb4, tb5,  solvingButton, solution, graphingButton
+            tb1, tb2, tb3, tb4,  solvingButton, graphingButton, resetButton
         ],
         width=300,
         spacing=10,
